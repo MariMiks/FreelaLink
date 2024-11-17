4b4c3eb37349d538aca45d7a43032e2f68a3aafb
Mariana Izumi <mariana.izumiks@gmail.com>
2024-11-17 09:45:51 -0300

    Iniciando back

--- Backend/main.ts
+++ Backend/main.ts
@@ -1,1 +1,20 @@
 require("dotenv-ts").config()
+
+import cors from "cors"
+import express  from "express"
+
+const PORT = (process.env.PORT ? parseInt(process.env.PORT) : 3001)
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(
+    PORT,
+    "0.0.0.0",
+    function () {
+      console.log(`API Freelalink aberta na porta ${PORT}`);
+    }
+  );
+
+  module.exports = app;
