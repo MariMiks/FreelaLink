f815e7b13f48f3dda8e92dccd64d8b8ea132053d
Mariana Izumi <mariana.izumiks@gmail.com>
2024-11-17 09:51:34 -0300

    Excluindo antigo back

--- backend/routes/servicoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const servicoControler = require('../controles/servicoControler');
-
-router.post('/cadastrar', servicoControler.addServico);
-router.get('/listarTodos', servicoControler.listTodosServico);
-router.get('/listarUm', servicoControler.listUmServico);
- 
-module.exports = router;
--- backend/routes/usuarioRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuarioControler = require('../controles/usuarioControler');
-
-router.post('/cadastrar', usuarioControler.addUsuario);
-router.get('/listarUm', usuarioControler.listUsuario);
-router.get('/listarTodos', usuarioControler.listTodosUsuario);
-
-module.exports = router;
