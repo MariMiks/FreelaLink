a8b3783c37847aedb4b81396ae8fd075a5662f66
Mariana Izumi <mariana.izumiks@gmail.com>
2024-11-17 09:08:41 -0300

    Iniciando backend

---
+++ backend/src/main.ts
@@ -0,0 +1,18 @@
+require("dotenv-ts").config();
+
+import cors from "cors";
+import express from "express";
+
+const PORT = (process.env.PORT ? parseInt(process.env.PORT) : 3001)
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(
+    PORT,
+    "0.0.0.0",
+    function () {
+        console.log(`API aberta na porta ${PORT}`);
+    }
+)
---
+++ backend/src/services/postgres.ts
@@ -0,0 +1,37 @@
+import { Pool, QueryResultRow } from "pg";
+
+function StartConnection(): Pool {
+    const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID, PGPORT } = process.env;
+
+    if (!PGHOST || !PGDATABASE || !PGUSER || !PGPASSWORD || !ENDPOINT_ID || !PGPORT) {
+        throw "Erro ao carregar variáveis de ambiente postgres";
+    }
+
+    return new Pool({
+        host: PGHOST,
+        user: PGUSER,
+        password: PGPASSWORD,
+        database: PGDATABASE,
+        port: parseInt(PGPORT),
+        ssl: true
+    });
+}
+
+function EndConnection(conn: Pool) {
+    conn.end();
+}
+
+async function Query<T extends QueryResultRow = any>(conn: Pool, query: string, valores: Array<any>): Promise<QueryResultRow> {
+    const result = await conn.query<T>(
+        query,
+        valores
+    );
+
+    return result;
+}
+
+export {
+    StartConnection,
+    EndConnection,
+    Query
+}
